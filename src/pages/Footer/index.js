04a3e21502d266b3c09cdb8e09ce9b8d8d437eae
Chris Pilson <Chris.Pilson@gmail.com>
2018-03-05 15:41:41 -0700

    Fixed Footer
    ~ Oops--fixed footer is what I was after here; fixed

--- src/pages/Footer/index.js
+++ src/pages/Footer/index.js
@@ -2,28 +2,11 @@ import React from 'react';
 // import Link from 'gatsby-link';
 
 const Footer = () => (
-  <footer className="footer">
-    <div
-      style={{
-        background: '#4682B4',
-        marginTop: '1.45rem'
-      }}
-    >
-      <div
-        style={{
-          margin: '0 auto',
-          padding: '2.5rem 2.5rem'
-        }}
-      >
-        <h4
-          style={{
-            margin: 0, color: 'white', textDecoration: 'none', textAlign: 'center'
-          }}
-        >&copy; Chris Pilson 2018
-        </h4>
-      </div>
-    </div>
-  </footer>
+  <div className="navbar navbar-default navbar-fixed-bottom">
+    <footer>
+      <p>&copy; Chris Pilson 2018</p>
+    </footer>
+  </div>
 );
 
 export default Footer;
